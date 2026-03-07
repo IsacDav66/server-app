fdb941c2452013286a589436db89ce8068c452fc
IsacDav <isadavid.caccha@gmail.com>
2026-03-07 18:43:16 -0500

    06:42

--- api/apps.js
+++ api/apps.js
@@ -12,10 +12,6 @@ const crypto = require('crypto');
 const sharp = require('sharp'); // 👈 Importamos Sharp para las imágenes
 const processImage = require('../middleware/processImage'); // Importar el middleware
 
-const metadata = await sharp(inputFile.path).metadata();
-const width = metadata.width;
-const height = metadata.height;
-
 // Función para generar la huella digital (MD5)
 function getHash(filePath) {
     return new Promise((resolve, reject) => {
@@ -447,9 +443,7 @@ router.post('/emojis/upload', protect, uploadStickerMiddleware, async (req, res)
             return res.status(200).json({ 
                 success: true, 
                 url: existingFilePath, 
-                filename: path.basename(existingFilePath),
-                width: width,   // 🚀 Enviamos ancho
-                height: height  // 🚀 Enviamos alto
+                filename: path.basename(existingFilePath) 
             });
         }
 
